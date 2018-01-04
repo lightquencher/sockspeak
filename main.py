2a95a394457e4394e2c76f37d36c49e9c3faa84b
lightquencher <lightquencher@gmail.com>
2018-01-04 19:30:11 +1300

    Update main.py

--- main.py
+++ main.py
@@ -5,20 +5,32 @@ import threading
 import lqsock
 import os
 
+# The function for the sending thread
 def send_thread(sock_used):
-    sock_used.send(raw_input("> "))
+    data = raw_input("> ")
+    sock_used.send(data)
+    if data == "sockquit":
+        sock_used.quit()
+        quit()
 
+# The function for the recieving thread
 def recv_thread(sock_used):
-    data = 'espeak "' + str(sock_used.listen()) + '"'
+    data = str(sock_used.listen())
+    if data == "sockquit":
+        sock_used.quit()
+        quit()
+    else:
+        data = 'espeak "' + data + '"'
     os.system(data)
 
+# Main
 def main():
     global mainsock
-    mainsock = lqsock.Sock("your-ip", "target-ip", 3083)
+    mainsock = lqsock.Sock("your-ip", "target-ip", 3083) # Change "your-ip" & "target-ip"
     mainsock.create()
     mainsock.try_conn()
 
-    # Connected
+    # Connected, and ready to send/recieve
 
     s_t = threading.Thread(target=send_thread, args=(mainsock,))
     r_t = threading.Thread(target=recv_thread, args=(mainsock,))
@@ -29,8 +41,10 @@ def main():
     s_t.join()
     r_t.join()
 
+# Run this until keyboard interrupt
 try:
     main()
 except KeyboardInterrupt as msg:
+    mainsock.send("sockquit")
     mainsock.quit()
     print "Done!"
